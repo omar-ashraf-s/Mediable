2da66ec30b94760785054cb3d6a0888bf4334a8a
Omar20Ashraf <o.ashraf@solutionplus.net>
2026-02-01 14:39:27 +0200

    wip: remove unused controler and routes

--- src/routes/mediable_routes.php
+++ src/routes/mediable_routes.php
@@ -1,7 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Mabrouk\Mediable\Http\Controllers\MediaController;
-
-Route::put('media/{medium}', [MediaController::class, 'update'])->name('media.update');
-Route::delete('media/{medium}', [MediaController::class, 'destroy'])->name('media.destroy');
